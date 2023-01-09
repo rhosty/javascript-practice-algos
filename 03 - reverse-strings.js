f41a1a0ece9c9f134b45e558faca86464aaaa388
rhosty <bernikulis@gmail.com>
2023-01-09 18:38:25 +0100

    03 done

--- 03 - reverse-strings.js
+++ 03 - reverse-strings.js
@@ -5,10 +5,14 @@
 // 2. 'word'   =>  'drow'
 
 function reverseString(string) {
-    // Write your code here
+    let reverseString = "";
+    for (let i = string.length - 1; i >= 0; i--){
+        reverseString += string[i];
+    }
+    return reverseString
 }
 
 
 // DO NOT TOUCH TESTS BELOW
-console.log(booleanToWord('world') === 'dlrow' ? '✅ 01 - Test Passed' : '❌ 01 - Test Failed');
-console.log(evenOrOdd('word') === 'drow' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
+console.log(reverseString('world') === 'dlrow' ? '✅ 01 - Test Passed' : '❌ 01 - Test Failed');
+console.log(reverseString('word') === 'drow' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
