ec9ca68c66d6d7d4eeda5ba36035063446ba3781
rhosty <bernikulis@gmail.com>
2023-01-09 18:32:47 +0100

    02 done

--- 02 - boolean-converter.js
+++ 02 - boolean-converter.js
@@ -2,11 +2,17 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function booleanToWord(boolean) {
-    // Write your code here
+    if(boolean === true){
+        return "Yes"
+    } else {
+        return "No"
+    }
 }
+
+
 
 
 
 // DO NOT TOUCH TESTS BELOW
 console.log(booleanToWord(true) === 'Yes' ? '✅ 01 - Test Passed' : '❌ 01 - Test Failed');
-console.log(evenOrOdd(false) === 'No' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
+console.log(booleanToWord(false) === 'No' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
